5f79144e65e5470cb7ae600c075011c35572dbb9
Domen Lanišnik <d.lanisnik@sportradar.com>
2017-08-08 23:02:16 +0200

    Initial project.

---
+++ app/src/main/java/si/lanisnik/restaurantorder/SplashActivity.kt
@@ -0,0 +1,12 @@
+package si.lanisnik.restaurantorder
+
+import android.app.Activity
+import android.os.Bundle
+
+class SplashActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
