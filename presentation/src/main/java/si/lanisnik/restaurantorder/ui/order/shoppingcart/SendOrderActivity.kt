da65f310a4fcb0f025905beda25255f50bb0c777
Domen Lanišnik <domen.lanisnik@gmail.com>
2017-11-18 11:52:27 +0100

    Created send order activity.

---
+++ presentation/src/main/java/si/lanisnik/restaurantorder/ui/order/shoppingcart/SendOrderActivity.kt
@@ -0,0 +1,38 @@
+package si.lanisnik.restaurantorder.ui.order.shoppingcart
+
+import android.os.Bundle
+import si.lanisnik.restaurantorder.R
+import si.lanisnik.restaurantorder.ui.base.BaseActivity
+
+/**
+ * Created by Domen Lanišnik on 18/11/2017.
+ * domen.lanisnik@gmail.com
+ */
+class SendOrderActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    override fun getContentView(): Int = R.layout.activity_send_order
+
+    override fun initToolbar() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initUi() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initViewModel() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setupObservers() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+}
